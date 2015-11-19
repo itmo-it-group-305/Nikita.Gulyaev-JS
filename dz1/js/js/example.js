b78118172f7e5babed26706e7be92829e28fb654
sqizzzzzz <gulyaevspb@gmail.com>
2015-11-19 16:36:57 +0300

    Первое ДЗ по JavaScript

---
+++ dz1/js/js/example.js
@@ -0,0 +1,13 @@
+var a = 9, b = 6;
+a = a + b;
+b = a - b;
+a = a - b;
+console.log(a,b) 
+
+var age=window.prompt('СКОЛЬКО ТЕБЕ ГОДИКОВ?');
+	if (age >= 60) {
+		alert('СТАРЫЙ ПЕРДУН!')
+	};
+	if (age < 60) {
+		alert('ОФИГЕТЬ КАКОЙ МОЛОДОЙ!')
+	};
